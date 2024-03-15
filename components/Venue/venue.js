c2ab68b1930b1f0aaae7f938830c086199f3c109
Shaurya Gupta <shauryag1000@gmail.com>
2024-03-15 23:43:20 +0530

    Added noopener

--- components/Venue/venue.js
+++ components/Venue/venue.js
@@ -1,17 +1,21 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import Link from 'next/link';
 
 function Venue({ className, city }) {
 	return (
-		<div>
+		<Link href={`/venue/${city.name}`}>
 			<div style={{'--image-url': `url(${city.img})`}} 
 			className='relative w-[300px] h-[400px] sm:w-[250px] sm:h-[350px] card-bg bg-[image:var(--image-url)] flex items-center justify-center p-4 cursor-pointer'>
 				<div className='flex justify-between flex-col w-full h-full'>
 					<div className='flex justify-between items-center'>
 						<div><div className={`border text-white text-md rounded-lg p-1 text-center mt-2 ${city.cfp ? 'block': 'hidden' }`}>cfp is open</div></div>
-						<a href={city.map} target='_blank' className='w-8 h-8 bg-white rounded-xl flex items-center justify-center' rel="noreferrer">
-						<img src='/img/mapIcon.svg' className='w-6' />
-						</a>
+						<button onClick={(e) => {
+							e.preventDefault()
+							window.open(city.map, '_blank', 'noopener')
+						}} className='w-8 h-8 bg-white rounded-xl flex items-center justify-center'>
+							<img src='/img/mapIcon.svg' className='w-6' />
+						</button>
 					</div>
 					<div></div>
 					<div className='text-white'>
@@ -22,9 +26,9 @@ function Venue({ className, city }) {
 						<div className='border border-gray-400 rounded-lg p-1 text-center mt-2'>{city.date}</div>
 						</div>
 					</div>
-					</div>
 				</div>
-		</div>
+			</div>
+		</Link>
 	);
 }
 
