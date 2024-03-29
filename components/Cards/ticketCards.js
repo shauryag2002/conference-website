2371dc61e2eae00c4dbd9871ea7169454499bf2b
Shaurya Gupta <shauryag1000@gmail.com>
2024-03-29 22:25:23 +0530

    Improve Home page

--- components/Cards/ticketCards.js
+++ components/Cards/ticketCards.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Button from '../Buttons/button'
 
-function TicketCards({className, city}) {
+function TicketCards({ className, city }) {
   return (
     <div className={`w-[300px] lg:w-full opacity-20 h-[400px] flex flex-col text-white justify-between rounded-lg card bg-white ${className}`} data-cy='ticketCard'>
-        <div className='p-4'>
-            <div className='text-xl font-bold text-gradient'>{city.name}, {city.country}</div>
-            <div className='mt-2 text-lg'>{city.date}</div>
-        </div>
-        <div className='border-t h-20 border-dashed p-4 text-center'><Button label='Buy now' disabled={true} className='w-[200px]'>Buy now</Button></div>
+      <div className='p-4'>
+        <div className='text-xl font-bold text-gradient'>{city.name}, {city.country}</div>
+        <div className='mt-2 text-lg'>{city.date}</div>
+      </div>
+      <div className='border-t h-20 border-dashed p-4 text-center'><Button label='Buy now' disabled={true} className='w-[200px] cursor-not-allowed'>Buy now</Button></div>
     </div>
   )
 }
